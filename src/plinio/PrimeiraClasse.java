18e62f31935c4115fb0f43b0a00f095d3790bc23
Enzo Fuji <enzo.juniti@gmail.com>
2020-05-30 16:08:08 -0300

    Create PrimeiraClasse.java
    Teste

---
+++ src/plinio/PrimeiraClasse.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package teste;
+
+/**
+ * @author enzoj
+ *
+ */
+public class PrimeiraClasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
