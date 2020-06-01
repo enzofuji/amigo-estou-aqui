19e854415d8f65b188c291ded137675ef9f937d2
gabiicarlos <66274646+gabiicarlos@users.noreply.github.com>
2020-06-01 16:36:39 -0300

    Update PrimeiraClasse.java
    Adicionei "People" na linha 17

--- src/plinio/PrimeiraClasse.java
+++ src/plinio/PrimeiraClasse.java
@@ -14,7 +14,7 @@ public class PrimeiraClasse {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hey!");
+		System.out.println("Hey People!!!");
 		System.out.println("#FiquemBem | #FiquemEmCasa");
 	}
 
