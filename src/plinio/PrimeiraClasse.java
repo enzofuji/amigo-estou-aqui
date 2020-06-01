b53b8c934d5eae55960af033a8aabf2796a7094a
Guilherme Cruz <guilhermecruz001@gmail.com>
2020-06-01 10:55:34 -0300

    Update PrimeiraClasse.java

--- src/plinio/PrimeiraClasse.java
+++ src/plinio/PrimeiraClasse.java
@@ -15,6 +15,7 @@ public class PrimeiraClasse {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hey!");
+		System.out.println("#FiquemBem | #FiquemEmCasa");
 	}
 
 }
