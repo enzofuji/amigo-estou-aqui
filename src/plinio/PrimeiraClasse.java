8b9ee3b0400382889226563ef2f783da038305fc
Gustavo <g236146@dac.unicamp.br>
2020-06-01 19:27:10 -0300

    Update PrimeiraClasse.java

--- src/plinio/PrimeiraClasse.java
+++ src/plinio/PrimeiraClasse.java
@@ -13,10 +13,10 @@ public class PrimeiraClasse {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Hey!");
-		System.out.println("Hi!");
-		System.out.println("#FiquemBem | #FiquemEmCasa");
+		System.out.println("mano");
+		System.out.println("socorro!");
+		System.out.println("nao sei oq eu to fazendo");
+		System.out.println("#FiquemBem | #eu nao vi as aulas de POO");
 		System.out.println("#WashYourHands| #StayAtHome | #BlackLivesMatter");
 
 	}
